0eb156563d1584c7197bf01da16ccc1463869f2d
tarunjangid12 <146462019+tarunjangid12@users.noreply.github.com>
2023-12-25 16:21:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,37 +8,37 @@ from config import Config
 from config import LOGGER
 
 from user import User
-from pyrogram import Client
 
-class Bot(Client):
+from telethon import TelegramClient
+
+class Bot(TelegramClient):
     USER = None
     USER_ID = None
 
     def __init__(self):
-        name = "my_bot"
-        session_name = "AQFplNMAt_g3Zo403aPpTa3f77dXHu6H0SOspgpCbdMNi18PvJRAnWRQEchkrx-_pItrk9wwIiSBpqI-NM7lBculnsPeLGcb6cd-L__9ZbmhGe4pBNwpLyRy4dTK_UNoAtoJb1YcCexcF-L59iv0SEYOlFlsRMp9FQ8iZS27K9OlVEOBUc-_3vMNMzr_vfSttITeL1Z1XJM4eQVflfgb1fDAhJ_dtsVvq7MRjIqdQtG_jPFvXQI9sPH7kudugQpe0tssa2CAUMuXAUfEP_FBEKkYTpb_3uJRJbt6YIGIa5S8v70j-Z6sD0zVOE1qJVYx36G7Z4dgN0tfYdJEBun5vBA3AD0P3AAAAAFI7DftAA"  # Add your desired session name here
+        api_id = 23696595  # Replace with your API ID
+        api_hash = 'a5b4f74cd5b550622c4eee4fea7285b0'  # Replace with your API hash
+        session_name = '1AZWarzoBu16jHqFBvPYZMWTX7q5E2ja075bMIUr21lxTR_FlpL85wyB4Lazrt0-Xw8ZVsGFddzlWTnVwhY7OeQfBCySjYW9NU7U6Mtw1Qlbx7rO9fH-lWOQiWEabafR6wW-Pekxrfsmct-dKcM-gvkQiDhtcU7h8YTJ2uDPWI2oOALuXLS6oZyvTmyJYZfh89GzFkp9exzMoWZFegM3eg48ea_YLfrCVvsbrdbK_tBq8InAM6DWZlhtzMfqwnspnmp13edEhSq2GqLM7oHSkIblTUY7sSxWAN_DPbtJ4rAlXDUDN6YmN9hdGg_OpHmUec3UQLHXJOh0jLK5jdErfHyU0RV6TO80='  # Add your desired session name here
+
         super().__init__(
             session_name,
-            api_hash="a5b4f74cd5b550622c4eee4fea7285b0",
-            api_id=23696595,  # Replace with your API ID
-            plugins={"root": "plugins"},
-            workers=4,
-            bot_token="YOUR_BOT_TOKEN"
+            api_id,
+            api_hash,
+            plugins=None
         )
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
         usr_bot_me = await self.get_me()
-        self.set_parse_mode("html")
-        self.LOGGER(__name__).info(
+        self.LOGGER(name).info(
             f"@{usr_bot_me.username} started!"
         )
-        self.USER, self.USER_ID = await User().start()
+        self.USER, self.USER_ID = await self.get_user_info()
 
     async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
+        await super().disconnect()
+        self.LOGGER(name).info("Bot stopped. Bye.")
 
 
 bot = Bot()
